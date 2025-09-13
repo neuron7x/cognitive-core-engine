428f0210a866d97e8a6ea5c3779bf909cb5fad5d
Yaroslav Vasylenko <stapkabenderka@ukr.net>
2025-09-13 12:47:38 +0300

    chore: add universal ci cd

---
+++ tests/java/src/test/java/app/SanityTest.java
@@ -0,0 +1,11 @@
+package app;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SanityTest {
+    @Test
+    void sanity() {
+        assertEquals(4, 2 + 2);
+    }
+}
